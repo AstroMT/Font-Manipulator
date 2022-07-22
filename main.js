8e0720c39b7df22e8ae82c54ab95b63655546b24
AstroMT <81778387+AstroMT@users.noreply.github.com>
2022-07-22 14:48:08 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-function preload() {};
+var noseX = 0;
+var noseY = 0;
+var leftWristX = 0;
+var rightWristX = 0;
+var difference = 0;
 
 function setup() {
     canvas = createCanvas(350,350);
     canvas.position(560, 175);
-    canvas.hide();
     video = createCapture(VIDEO);
     video.size(350, 350);
-
-    posenet = ml5.poseNet(video, modelLoaded);
-    posenet.on("pose", gotPoses);
+ 
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
 }
 
 function modelLoaded() {
@@ -18,7 +21,20 @@ function modelLoaded() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+ 
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        console.log("Left wrist X coordinate is " + leftWristX + " and the right wrist X coordinate is " + rightWristX);
+        difference = floor(leftWristX - rightWristX);
     }
 }
 
-function draw() {};
+function draw() {
+    background("lightgrey");
+
+    textSize(difference);
+    fill("pink");
+    text("Medha", 50, 200);
+
+    document.getElementById("text-font-size").innerHTML = difference;
+}
